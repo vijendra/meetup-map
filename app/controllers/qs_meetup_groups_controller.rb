479a10e16c33c8be0075962892eae3e5db521f1e
Sacha Chua <sacha@sachachua.com>
2012-11-17 13:19:25 -0500

    Tweak display of Google Maps, group detail page

--- app/controllers/qs_meetup_groups_controller.rb
+++ app/controllers/qs_meetup_groups_controller.rb
@@ -1,7 +1,12 @@
 class QsMeetupGroupsController < ApplicationController
   def index
     @quantifiers = QsMeetupGroup.order(:place).all
-    @markers = @quantifiers.to_gmaps4rails   
+    @markers = @quantifiers.to_gmaps4rails do |group, marker|
+      marker.title   group.name
+      marker.infowindow render_to_string(:partial => "/qs_meetup_groups/detail", :locals => { :group => group})
+      marker.json({ :id => group.id })
+#      marker.title "name"
+    end
   end
 
   def show
