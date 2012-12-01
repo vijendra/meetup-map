d1a206b0b2f141d2a671955d3688f17bce5a24bd
vijendra <vijendrakarkala@gmail.com>
2012-12-01 22:57:00 +0530

    tweet fetcher -bugfix

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
   def index
-    @tweets = Tweet.order('tweeted_on desc').where('ancestry is null').page(params[:page]).per(50)
+    @tweets = Tweet.order('tweeted_on desc').where('ancestry is null').where('tweeted_on is not null').page(params[:page]).per(50)
     #@tweets = Tweet.tweets_with_replies.page(params[:page]).per(50)
   end
 end
